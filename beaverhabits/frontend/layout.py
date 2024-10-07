635e64d6622311625b58b120171811469f547545
daya0576 <daya0576@gmail.com>
2024-10-07 12:00:26 +0800

    Add web app support for iOS

--- beaverhabits/frontend/layout.py
+++ beaverhabits/frontend/layout.py
@@ -1,12 +1,31 @@
 from contextlib import contextmanager
 import os
 
-from nicegui import ui
+from nicegui import app, ui
 
 from beaverhabits.app.auth import user_logout
 from beaverhabits.frontend import icons
 from beaverhabits.frontend.components import compat_menu, menu_header, menu_icon_button
 from beaverhabits.storage.meta import get_page_title, get_root_path
+
+
+def custom_header():
+    app.add_static_files("/images", "images")
+
+    ui.add_head_html(
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">'
+    )
+    ui.add_head_html('<meta name="apple-mobile-web-app-title" content="Beaver">')
+    ui.add_head_html('<meta name="apple-mobile-web-app-capable" content="yes">')
+    ui.add_head_html(
+        '<meta name="apple-mobile-web-app-status-bar-style" content="black">'
+    )
+    ui.add_head_html('<meta name="theme-color" content="#121212">')
+
+    # viewBox="90 90 220 220"
+    ui.add_head_html(
+        '<link rel="apple-touch-icon" href="/images/apple-touch-icon-v4.png">'
+    )
 
 
 def menu_component(root_path: str) -> None:
@@ -29,15 +48,18 @@ def menu_component(root_path: str) -> None:
 
 
 @contextmanager
-def layout(title: str | None = None):
+def layout(title: str | None = None, with_menu: bool = True):
     """Base layout for all pages."""
     root_path = get_root_path()
     title = title or get_page_title(root_path)
     with ui.column().classes("max-w-sm mx-auto sm:mx-0"):
+        custom_header()
+
         with ui.row().classes("min-w-full"):
             menu_header(title, target=root_path)
-            ui.space()
-            with menu_icon_button(icons.MENU):
-                menu_component(root_path)
+            if with_menu:
+                ui.space()
+                with menu_icon_button(icons.MENU):
+                    menu_component(root_path)
 
         yield
