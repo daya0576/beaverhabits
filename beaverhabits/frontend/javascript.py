011035107cf8a25c8e74a78e6092eb2671436486
daya0576 <daya0576@gmail.com>
2025-01-21 06:48:14 +0800

    Bump up nicegui

--- beaverhabits/frontend/javascript.py
+++ beaverhabits/frontend/javascript.py
@@ -11,6 +11,23 @@ document.addEventListener('contextmenu', function(event) {
 });
 """
 
+UNHOVER_CHECKBOXES = """\
+const elements = document.querySelectorAll('.q-checkbox');
+console.log(elements);
+
+elements.forEach(element => {
+  const mouseOutEvent = new Event('mouseout');
+  element.addEventListener('mouseout', () => {
+    console.log('mouseout');
+    element.dispatchEvent(mouseOutEvent);
+  });
+});
+"""
+
 
 def prevent_context_menu():
     ui.run_javascript(PREVENT_CONTEXT_MENU)
+
+
+def unhover_checkboxes():
+    ui.run_javascript(UNHOVER_CHECKBOXES)
