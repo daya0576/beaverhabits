dda285e2b2bb80fae8834e207772e68423b73cb3
Benjamin Kobjolke <b.kobjolke@xida.de>
2025-04-02 08:12:12 +0200

    FEATURE: password reset script

--- utils/reset_password.bat
+++ utils/reset_password.bat
@@ -1,6 +1,9 @@
 @echo off
+echo Changing to project root directory...
+cd /d "%~dp0.."
+
 echo Activating virtual environment...
-call ..\.venv\Scripts\activate.bat
+call .venv\Scripts\activate.bat
 
 echo Running password reset script...
 python -m utils.reset_password
